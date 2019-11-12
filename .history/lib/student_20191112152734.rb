b2e31963c95af698548c97cfc301f18c8de36a1c
PBillingsby <philbillingsby@gmail.com>
2019-11-12 15:35:35 -0500

    Done.

--- .history/lib/student_20191112152734.rb
+++ .history/lib/student_20191112152734.rb
@@ -1,9 +1,11 @@
 class Student
+
   def hello 
     puts "Hey there! I'm so excited to learn stuff.\n"
   end
 
   def raise_hand
-    puts "Pick me!\n"
+    puts "Pick me!"
   end
+
 end
---
+++ .history/lib/student_20191112152927.rb
@@ -0,0 +1,9 @@
+class Student
+  def hello 
+    return "Hey there! I'm so excited to learn stuff.\n"
+  end
+
+  def raise_hand
+    return "Pick me!\n"
+  end
+end
