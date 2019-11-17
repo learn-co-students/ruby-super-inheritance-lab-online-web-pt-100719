fd05b1d9224225175a84dae0145725a9956cbf7c
Derric Fragosa <fderric@gmail.com>
2019-11-17 08:27:25 +0000

    Done.

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,18 +4,13 @@ class ChattyStudent < Student
 
 	def hello
 		super
-		puts "How are you doing today? I'm okay, but I'm kind of tired. 
-		Did you watch The Walking Dead last night? 
-		You didn't?! Oh man, it was so crazy! 
-		What, you don't want any spoilers? 
-		Okay well let me just tell you who died..."
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
 	end
 	
 	def raise_hand
-		phrase = "Pick me!"
+		
 		10.times do 
-		puts phrase
+		  super
+		end 
 	end 
-	
 end 
-
